a8aa2559a781d37b9d79f9fb8eaf96b3d030cd4b
Nick Ma <nick.ma@cerner.com>
2020-05-11 00:02:44 -0500

    daily update

--- modal.js
+++ modal.js
@@ -1,33 +1,3 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
 function open_image(filename) {
-  modal.style.display = "block";
-  modalImg.src = filename;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal_close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-
-modal.onclick = function() {
-  modal.style.display = "none";
-}
-
-modalImg.onclick = function() {
-}
-
-document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    if (evt.keyCode == 27) {
-        modal.style.display = "none";
-    }
+  parent.open_image(filename);
 };
